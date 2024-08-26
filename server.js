b11dad04789df4ac539bc8cb4969478e091de39a
Rohan Vinkare <rohanvinkare2022@gmail.com>
2024-08-26 19:29:52 +0530

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const schoolRoutes = require('./routes/schoolRoutes');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
+// Middleware to parse URL-encoded and JSON bodies
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(schoolRoutes);
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
